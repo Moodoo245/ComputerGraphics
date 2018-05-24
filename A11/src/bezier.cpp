cb7d096540b8db317d25dce5f5d430a58ec201a1
Sebyby <sebastien.chevaleyre@epfl.ch>
2018-05-24 07:28:23 +0200

    bezier

--- A11/src/bezier.cpp
+++ A11/src/bezier.cpp
@@ -13,7 +13,16 @@ vec3 PiecewiseBezier::eval_bezier(int bp_offset, float t) const {
      * Evaluate the cubic Bezier polygon defined by control points
      * bezier_control_points_[bp_offset..bp_offset + 4] at parameter t in [0, 1]
      **/
-    return bezier_control_points_[bp_offset];
+  std::vector <std::vector<vec3> > bezierMatrix(4, std::vector<vec3>(4, vec3(0,0,0)));
+  for(int i = 0; i < 4; i++){
+    bezierMatrix[0][i] = bezier_control_points_[bp_offset+i];
+  }
+  for(int i = 1; i < 4; i++){
+    for(int j = 0; j < 4-i; j++){
+      bezierMatrix[i][j] = (1-t)*bezierMatrix[i-1][j] + t*bezierMatrix[i-1][j+1];
+    }
+  }
+  return bezierMatrix[3][0];
 }
 
 // Calculate a tangent at point at one of the Bezier curve segments
@@ -29,7 +38,16 @@ vec3 PiecewiseBezier::eval_bezier_tangent(int bp_offset, float t) const {
      * Recall, the tangent vector for a curve c(t) is given by the derivative of
      * this curve, c'(t).
      **/
-    return vec3(1, 0, 0);
+    std::vector <std::vector<vec3> > bezierMatrix(3, std::vector<vec3>(4, vec3(0,0,0)));
+    for(int i = 0; i < 4; i++){
+      bezierMatrix[0][i] = bezier_control_points_[bp_offset+i];
+    }
+    for(int i = 1; i < 3; i++){
+      for(int j = 0; j < 4-i; j++){
+	bezierMatrix[i][j] = (1-t)*bezierMatrix[i-1][j] + t*bezierMatrix[i-1][j+1];
+      }
+    }
+    return 3.0*(bezierMatrix[2][1]-bezierMatrix[2][0]);
 }
 
 std::vector<vec3> PiecewiseBezier::control_polygon_to_bezier_points(std::vector<vec3> const& cp) {
@@ -45,9 +63,22 @@ std::vector<vec3> PiecewiseBezier::control_polygon_to_bezier_points(std::vector<
      *                    segment 2 should be controlled by bezier_pts 6, 7, 8, 9;
      * and so on.
      **/
-    size_t numSegments = cp.size() - 3;
-    bezier_pts.resize(3 * numSegments + 1);
+    size_t numSegments = cp.size()-3;
+    bezier_pts.resize(3*numSegments + 1 );
 
+    for(int i = 0; i < numSegments-1; i++) {
+      bezier_pts[3*i+2] = (cp[i] + 2.0*cp[i+1]) / 3.0;
+      bezier_pts[3*i+4] = (cp[i+2] + 2.0*cp[i+1]) / 3.0;
+      bezier_pts[3*i+3] = (bezier_pts[3*i+2] + bezier_pts[3*i+4]) / 2.0; 
+    }
+    bezier_pts[3*numSegments - 4] = (cp[numSegments-2] + 2.0*cp[numSegments-1]) / 3.0;
+    bezier_pts[3*numSegments - 2] = (2.0*cp[numSegments-1] + cp[0]) / 3.0;
+    bezier_pts[3*numSegments - 3] = (bezier_pts[3*numSegments - 4] + bezier_pts[3*numSegments - 2]) / 2.0;
+    bezier_pts[1] = (cp[1] + 2.0*cp[0]) / 3.0;
+    bezier_pts[3*numSegments - 1] = (cp[numSegments-1] + 2.0*cp[0]) / 3.0;
+    bezier_pts[0] = (bezier_pts[1] + bezier_pts[3*numSegments - 1]) / 2.0;
+    bezier_pts[3*numSegments] = bezier_pts[0];
+    
     return bezier_pts;
 }
 
@@ -60,8 +91,9 @@ vec3 PiecewiseBezier::eval_piecewise_bezier_curve(float t) const {
      *  such that you evaluate the correct point on the correct Bezier segment. Note that
      *  the current code only evaluates the very first Bezier segment.
      */
-
-    return eval_bezier(0, t);
+  int n = num_segments();
+  int offset = floor(n*t);
+  return eval_bezier(3*offset,n*(t-offset/((float)n)));
 }
 
 vec3 PiecewiseBezier::operator()(float t) const {
@@ -78,8 +110,9 @@ vec3 PiecewiseBezier::tangent(float t) const {
      *  the current code only evaluates the very first Bezier segment.
      *  Also, remember to use the chain rule when computing the tangent!
      */
-
-    return eval_bezier_tangent(0, t);
+  int n = num_segments();
+  int offset = floor(n*t);
+  return n*eval_bezier_tangent(3*offset,n*(t-offset/((float)n)));
 }
 
 void PiecewiseBezier::set_control_polygon(const std::vector<vec3> &control_polygon, bool loop) {
